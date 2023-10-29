c653aa9da7ca0af8ebf5777ad72382050321b8a4
william <wvmwill@gmail.com>
2023-10-29 20:30:00 -0300

    salvando split depois de dois dias sofrendo

--- libft.h
+++ libft.h
@@ -13,9 +13,8 @@
 #ifndef LIBFT_H
 # define LIBFT_H
 
-# include <stdlib.h>
-# include <stddef.h>
-# include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
